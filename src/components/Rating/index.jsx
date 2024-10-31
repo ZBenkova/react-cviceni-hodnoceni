89635204761cec2ee9a32baf0297823a89cee101
Zuzana Benkova <zuzanka.dvorska@gmail.com>
2024-10-31 21:23:04 +0100

    hotovy ukol

--- src/components/Rating/index.jsx
+++ src/components/Rating/index.jsx
@@ -1,19 +1,23 @@
-import { useState } from 'react';
-import { Star } from '../Star';
-import './style.css';
+import { useState } from "react";
+import { Star } from "../Star";
+import "./style.css";
 
 export const Rating = () => {
   const [rating, setRating] = useState(0);
-  
+
+  const setRatingValue = (value) => {
+    setRating(value);
+  };
+
   return (
     <div className="rating">
       <div className="rating__value">{rating}</div>
       <div className="rating__stars">
-        <Star />
-        <Star />
-        <Star />
-        <Star />
-        <Star />
+        <Star value={1} onSelect={setRatingValue} glowing={rating >= 1} />
+        <Star value={2} onSelect={setRatingValue} glowing={rating >= 2} />
+        <Star value={3} onSelect={setRatingValue} glowing={rating >= 3} />
+        <Star value={4} onSelect={setRatingValue} glowing={rating >= 4} />
+        <Star value={5} onSelect={setRatingValue} glowing={rating >= 5} />
       </div>
     </div>
   );
--- src/components/Star/index.jsx
+++ src/components/Star/index.jsx
@@ -1,14 +1,11 @@
-import { useState } from 'react';
-import './style.css';
+import "./style.css";
 
-export const Star = () => {
-  const [glowing, setGlowing] = useState(false);
-  
-  const starClass = glowing
-    ? 'rating__star rating__star--on'
-    : 'rating__star';
+export const Star = ({ value, onSelect, glowing }) => {
+  const starClass = glowing ? "rating__star rating__star--on" : "rating__star";
 
-  return (
-    <div className={starClass}></div>
-  );
+  const clickHandler = () => {
+    onSelect(value);
+  };
+
+  return <div className={starClass} onClick={clickHandler}></div>;
 };
